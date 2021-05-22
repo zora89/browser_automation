717ec53e7f0c6b4823d76441741713db34501a99
zora89 <zorawar.purohit@gmail.com>
2021-05-22 21:55:25 +0530

    got the time check inside while loop

--- automation_nike.py
+++ automation_nike.py
@@ -11,18 +11,22 @@ import datetime as dt
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
-tg_day = dt.datetime(2021, 5, 22, 21, 32, 0, 1)
+tg_day = dt.datetime(2021, 5, 22, 21, 54, 0, 1)
 print(tg_day)
-date = dt.datetime.now()
-print(date)
 
 def while_1():
-        if tg_day.hour == date.hour and tg_day.minute == date.minute:
-            nike_website() # this breaks out of the while loop if it's the right day.
-        else:
-            print("Waiting a bit longer....")
-            sl(1) #wait 1 seconds
+    date = dt.datetime.now()
+    print(date)
 
+    if tg_day.hour == date.hour and tg_day.minute == date.minute:
+        print("Activated")
+        nike_website() # this breaks out of the while loop if it's the right day.
+
+    else:
+        print("Waiting a bit longer....")
+        sl(1) #wait 1 seconds
+        while_1()    
+    
 def nike_website():
 
     driver.get('https://gs.nike.com/?checkoutId=47fa2977-c2f4-46ad-bd7d-384fddf2f70c&launchId=e18f55bb-c188-4fab-83c0-cc19107f59e7&skuId=0fef8cb6-f185-5ced-bffd-c09dd2575521&country=IN&locale=en-GB&appId=com.nike.commerce.snkrs.web&returnUrl=https:%2F%2Fwww.nike.com%2Fin%2Flaunch%2Ft%2Flebron-18-low-mimi-plange-higher-learning%2F')
