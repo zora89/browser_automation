9494c8d05c540924a7f4c887598bc587c93831f5
zora89 <zorawar.purohit@gmail.com>
2021-05-22 21:34:14 +0530

    furthered with datetime and selenium

--- automation_nike.py
+++ automation_nike.py
@@ -1,28 +1,54 @@
 #automating nike purchase for Sankalp
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep as sl
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
+import datetime as dt
 
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
-driver.get('https://gs.nike.com/?checkoutId=47fa2977-c2f4-46ad-bd7d-384fddf2f70c&launchId=e18f55bb-c188-4fab-83c0-cc19107f59e7&skuId=0fef8cb6-f185-5ced-bffd-c09dd2575521&country=IN&locale=en-GB&appId=com.nike.commerce.snkrs.web&returnUrl=https:%2F%2Fwww.nike.com%2Fin%2Flaunch%2Ft%2Flebron-18-low-mimi-plange-higher-learning%2F')
-print(driver.title)
-sl(3)
+tg_day = dt.datetime(2021, 5, 22, 21, 32, 0, 1)
+print(tg_day)
+date = dt.datetime.now()
+print(date)
 
-driver.switch_to.default_content()
-wait = WebDriverWait(driver,10)
-wait.until(EC.element_to_be_clickable((By.NAME, 'emailAddress')))
+def while_1():
+        if tg_day.hour == date.hour and tg_day.minute == date.minute:
+            nike_website() # this breaks out of the while loop if it's the right day.
+        else:
+            print("Waiting a bit longer....")
+            sl(1) #wait 1 seconds
 
-email = driver.find_element_by_name("emailAddress")
-email.send_keys("zorawar.purohit@gmail.com")
-email.send_keys(Keys.RETURN)
+def nike_website():
+
+    driver.get('https://gs.nike.com/?checkoutId=47fa2977-c2f4-46ad-bd7d-384fddf2f70c&launchId=e18f55bb-c188-4fab-83c0-cc19107f59e7&skuId=0fef8cb6-f185-5ced-bffd-c09dd2575521&country=IN&locale=en-GB&appId=com.nike.commerce.snkrs.web&returnUrl=https:%2F%2Fwww.nike.com%2Fin%2Flaunch%2Ft%2Flebron-18-low-mimi-plange-higher-learning%2F')
+    print(driver.title)
+
+    driver.switch_to.default_content()
+    wait = WebDriverWait(driver,10)
+    wait.until(EC.element_to_be_clickable((By.NAME, 'emailAddress')))
+
+    email = driver.find_element_by_name("emailAddress")
+    email.send_keys("zorawar.purohit@gmail.com")
+    email.send_keys(Keys.RETURN)
+
+    password = driver.find_element_by_name("password")
+    password.send_keys("123456xZ")
+    password.send_keys(Keys.RETURN)
+
+    sl(10)
 
 
-password = driver.find_element_by_name("password")
-password.send_keys("123456xZ")
-password.send_keys(Keys.RETURN)
+while True:
+    while_1()
+
+
+
+
+
+
 
--- google_search.py
+++ google_search.py
@@ -5,15 +5,22 @@ from time import sleep as sl
 
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
+x1 = webdriver.Chrome(PATH)
 
 driver.get('https://google.com')
 print(driver.title)
-sl(3)
 
 email = driver.find_element_by_name("q")
 email.send_keys("Covid Vaccine")
 email.send_keys(Keys.RETURN)
 
+sl(10)
 
-#sl(10)
-#driver.quit()
+# Load new page 
+x1.get('https://google.com')
+email = x1.find_element_by_name("q")
+email.send_keys("Andaman News")
+email.send_keys(Keys.RETURN)
+
+
+
